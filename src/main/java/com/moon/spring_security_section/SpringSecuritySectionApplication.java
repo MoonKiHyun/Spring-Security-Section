db2bbd86cc8b62da16cded979063c667c755dbfe
Kihyun Moon <moonkihyun95@gmail.com>
2024-05-25 06:17:47 +0900

    git init

---
+++ src/main/java/com/moon/spring_security_section/SpringSecuritySectionApplication.java
@@ -0,0 +1,13 @@
+package com.moon.spring_security_section;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecuritySectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecuritySectionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/moon/spring_security_section/SpringSecuritySectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moon.spring_security_section;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecuritySectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
